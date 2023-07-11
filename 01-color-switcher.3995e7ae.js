99dfa0434f8faef52e991c1f120962fb173c3dba
NeoZLoy <123673700+NeoZLoy@users.noreply.github.com>
2023-07-11 06:17:51 +0000

    Deploying to gh-pages from @ NeoZLoy/goit-js-hw-09@176d12265afc5c3adcbcd9d61c6055a320de609f 🚀

--- 01-color-switcher.3995e7ae.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("body");function e(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}var a=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),d=null;n.disabled=!0,a.addEventListener("click",function(t){d=setInterval(e,1e3),a.disabled=!0,n.disabled=!1}),n.addEventListener("click",function(t){clearInterval(d),a.disabled=!1,n.disabled=!0})}();
-//# sourceMappingURL=01-color-switcher.3995e7ae.js.map
